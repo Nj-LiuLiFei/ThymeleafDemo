bbfd70ee5a3ba9e6bd6a9e41954b965fe6c0477c
LiuLiFei <1061334689@qq.com>
2018-09-07 19:05:50 +0800

    2018-9-7 19:05:36

---
+++ src/main/java/com/example/demo/controller/AuthorizationController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@RestController
+@RequestMapping(value = "service/api/",produces="text/javascript;charset=UTF-8")
+public class AuthorizationController {
+
+    @GetMapping("/getAuth")
+    public String getAuth(){
+        return "var auth = \"liulifei\";alert(\"你好呀\");";
+
+    }
+}
---
+++ src/main/resources/static/js/l-ui.js
@@ -0,0 +1,77 @@
+;"use strict";
+
+/* 结构树插件 */
+(function (window) {
+    function initTree(cof) {
+        var statrtTime = new Date().getTime();
+        this.ulNode = document.getElementById(cof.id);
+        this.data=cof.data;
+        this.renderNode();
+        bindNodeEvent.nodeExpand(this.ulNode);
+        console.log(cof.id+"结构树初始化："+(new Date().getTime()-statrtTime)+"ms");
+        return "";
+    };
+    var pt=initTree.prototype;
+    pt.renderNode=function(c,e){
+        var _ = this;
+        var data = c == undefined?_.data:c,
+            w = e == undefined?_.ulNode:e;
+        for (var i=0;i<data.length;i++){
+            var li = document.createElement("li"),
+                i0 = document.createElement("i"),
+                a  = document.createElement("a"),
+                i1 = document.createElement("i"),
+                i2 = document.createElement("i"),
+                span = document.createElement("span"),
+                ul = document.createElement("ul");
+            i0.className = "ztree-icon ztree-icon-default ztree-icon-none";
+            i1.className = "ztree-icon  ztree-icon-default ztree-icon-checkbox";
+            i2.className = "ztree-icon fa fa-folder";
+            span.innerHTML = data[i].name;
+            if(i == data.length-1){
+                li.className = "li-last";
+            }
+            if(data[i].children.length > 0) {
+                i0.className = "ztree-icon ztree-icon-default ztree-icon-close";
+                _.renderNode(data[i].children,ul);
+            }
+            a.appendChild(i1),a.appendChild(i2),a.appendChild(span),li.appendChild(i0),li.appendChild(a),li.appendChild(ul);
+            w.appendChild(li);
+        }
+    };
+
+    var bindNodeEvent={
+        "nodeExpand":function (ul) {
+            ul.addEventListener("click",function (ev) {
+                if(ev.target.className.indexOf("ztree-icon-open")>-1){
+                    ev.target.classList.remove("ztree-icon-open"),ev.target.classList.add("ztree-icon-close");
+                    var children = ev.target.parentNode.childNodes;
+                    for (var i=0;i<children.length;i++){
+                        if(children[i].nodeName == "UL"){
+                            children[i].style.display="block";
+                            break;
+                        }
+                    }
+                }else if(ev.target.className.indexOf("ztree-icon-close")>-1){
+                    ev.target.classList.remove("ztree-icon-close"),ev.target.classList.add("ztree-icon-open");
+                    var children = ev.target.parentNode.childNodes;
+                    for (var i=0;i<children.length;i++){
+                        if(children[i].nodeName == "UL"){
+                            children[i].style.display="none";
+                            break;
+                        }
+                    }
+                }
+            });
+        },
+    };
+
+    if(window.initTreePlugIn == undefined) {
+        window.initTreePlugIn=function (cof) {
+            return new initTree(cof);
+        };
+    }else {
+        console.error("结构树插件加载失败！window.initTreePlugIn 方法重复");
+    }
+    var ztreeList = document.getElementsByClassName("");
+})(window);
